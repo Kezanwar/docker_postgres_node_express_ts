dc7aca5f5dd9c68d03191bb5fcfe7b286827af56
Kez Anwar <kezanwar@gmail.com>
2024-10-18 23:20:55 +0100

    feature/auth - updated comments in auth router

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,19 +1,23 @@
 import express from "express";
+import AuthValidation from "@app/validation/auth";
 import AuthController from "@app/controllers/auth";
 import validate from "@app/middleware/validate";
-import AuthValidation from "@app/validation/auth";
 import auth from "@app/middleware/auth";
 
 const AuthRouter = express.Router();
 
-/**@public /api/auth/registers - registers a new user */
+/** @public */
+/** @endpoint /api/auth/registers */
+/** @desc registers a new @user */
 AuthRouter.post(
   "/register",
   validate(AuthValidation.registerSchema),
   AuthController.register
 );
 
-/**@private /api/auth/otp/<code> - confirms users email via OTP */
+/** @private */
+/** @endpoint /api/auth/otp/<@code> */
+/** @desc confirms users email via @otp */
 AuthRouter.post("/otp/:code", auth, AuthController.validateOTP);
 
 // AuthRouter.get("/otp", auth, AuthController.validateOTP);
